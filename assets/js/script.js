e9c1e27e13e943481258c7bdf44235f46f685a3e
Euthyphro <lulu.cao.her@gmail.com>
2022-06-05 11:08:52 -0500

    redesign navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,14 @@ navItems.addEventListener("mouseover", (e) => {
   navItems.style.setProperty("--x", e.clientX - x);
   navItems.style.setProperty("--y", e.clientY - y);
 });
+
+const navIcon = document.getElementById("navIcon");
+navIcon.addEventListener("click", navShow);
+function navShow() {
+  const navBar = document.querySelector("nav");
+  if (navBar.style.display == "flex") {
+    navBar.style.display = "none";
+  } else {
+    navBar.style.display = "flex";
+  }
+}
