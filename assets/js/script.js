b7cbe2749305f0f31bb97591e3c18305098fc42d
Euthyphro <lulu.cao.her@gmail.com>
2022-06-09 12:02:23 -0500

    minor style change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,32 @@
-const navItems = document.querySelector("body");
-navItems.addEventListener("mouseover", (e) => {
-  const { x, y } = navItems.getBoundingClientRect();
-  console.log("clientX is " + e.clientX);
-  console.log("clientY is " + e.clientY);
-  console.log("X is " + x);
-  console.log("Y is " + y);
-  console.log(e.clientX - x);
-  console.log(e.clientY - y);
-  navItems.style.setProperty("--x", e.clientX - x);
-  navItems.style.setProperty("--y", e.clientY - y);
-});
+// create a star following mouse movement
+// const navItems = document.querySelector("body");
+// navItems.addEventListener("mouseover", (e) => {
+//   const { x, y } = navItems.getBoundingClientRect();
+//   console.log("clientX is " + e.clientX);
+//   console.log("clientY is " + e.clientY);
+//   console.log("X is " + x);
+//   console.log("Y is " + y);
+//   console.log(e.clientX - x);
+//   console.log(e.clientY - y);
+//   navItems.style.setProperty("--x", e.clientX - x);
+//   navItems.style.setProperty("--y", e.clientY - y);
+// });
 
 // show detailed navbar on click for small screens
 const navBar = document.querySelector("nav");
+const header = document.querySelector("header");
 const navIcon = document.getElementById("navIcon");
 const navIconX = document.getElementById("navIconX");
 
 function navShow() {
-  navBar.style.display = "flex";
+  header.style.height = "5rem";
+  navBar.style.display = "block";
   navIconX.style.display = "block";
   navIcon.style.display = "none";
 }
 
 function navHide() {
+  header.style.height = "2.5rem";
   navBar.style.display = "none";
   navIconX.style.display = "none";
   navIcon.style.display = "block";
